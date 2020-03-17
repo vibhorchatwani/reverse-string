c3e44e485393432bef522abedda10659646dd29e
vibhorchatwani <59525358+vibhorchatwani@users.noreply.github.com>
2020-03-17 16:08:59 +0530

    Add files via upload

---
+++ rev_str.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+st="Core Python"
+n=len(st)
+i=0
+while i<n:
+    print(st[i],end=' ')
+    i+=1
+
+print()
+i=1
+while i<=n:
+    print(st[-i],end=' ')
+    i+=1
+
+
+# In[ ]:
+
+
+
+
